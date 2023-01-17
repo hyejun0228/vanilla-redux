e08a900a793415936ebcbc92dc9774dbf62c7f3c
김혜준 <alice5855@naver.com>
2023-01-18 04:42:56 +0900

    0118

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const countModifier = ( ) => {
+  return state;
+};
+
+const countStore = createStore();
+
+console.log(countStore.getState())
