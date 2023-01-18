ecfb92a9b46815630767a8227c628d46fab95c46
김혜준 <alice5855@naver.com>
2023-01-18 22:12:49 +0900

    0118-2

--- src/index.js
+++ src/index.js
@@ -3,11 +3,36 @@ import { createStore } from "redux";
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
-
-const countModifier = ( ) => {
-  return state;
+//getElementById : id으로 HTML불러올 수 있음
+//querySelector : 태그로 HTML 불러올 수 있음
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD"){
+    return count + 1;
+  }else if(action.type === "MINUS"){
+    return count - 1;
+  }else { 
+    return count;
+  }
 };
 
-const countStore = createStore();
+const countStore = createStore(countModifier);
 
-console.log(countStore.getState())
+const onChange = () => {
+  number.innerText =countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({type: "ADD"});
+};
+
+const handleMinus = () => {
+  countStore.dispatch({type:"MINUS"});
+};
+
+add.addEventListener("click",handleAdd);
+minus.addEventListener("click",handleMinus);
+
+
+
